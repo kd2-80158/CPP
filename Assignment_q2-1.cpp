20556cd2499340e8d476fcb05e227dfc20992ce2
kd2-80158 <chhimwalsaurabh@gmail.com>
2023-09-22 22:58:53 +0530

    Assigment 2 part 1 uploaded

---
+++ Assignment_q2-1.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+
+struct Date
+{
+   //data members
+   int day;
+   int month;
+   int year; 
+
+   //member functions
+   void initDate()
+   {
+    day=10;
+    month=10;
+    year=2023;
+   }
+void printDateOnConsole()
+{
+   printf("%d-%d-%d",day,month,year);
+}
+void acceptDateOnConsole()
+{ 
+   scanf("%d",&day);
+   scanf("%d",&month);
+   scanf("%d",&year);
+}
+bool isLeapYear()
+   {
+    int result=year;
+    if(result%4==0 || result%400==0)
+    {
+      return true;
+    }
+    else
+    {
+      return false; 
+    }  
+}
+};
+int main()
+{
+    int i;
+    struct Date ptrDate;
+    do
+    {
+        printf("\n");
+        printf("1.Initialize Date\n");
+        printf("2.Print Date\n");
+        printf("3.Accept Date\n");
+        printf("4.Check whether the given year is a leap year\n");
+        printf("\n");
+
+        printf("Enter your choice\n");
+        scanf("%d",&i);
+        switch(i)
+        {
+            case 1:
+             ptrDate.initDate();
+             break;
+
+            case 2:
+             ptrDate.printDateOnConsole();
+             break;
+
+            case 3:
+             ptrDate.acceptDateOnConsole();
+             break;
+
+            case 4:
+             bool x;
+             x=ptrDate.isLeapYear();
+             printf("%d",x);
+             break;
+            
+            default:
+            printf("Please enter the correct value\n");
+            break;
+
+        }
+
+    } while (i!=0);
+    
+    return 0;
+}
